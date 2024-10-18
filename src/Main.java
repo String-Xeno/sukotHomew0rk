f0cfd536b8264723c77999402d46bdbbc0f6b5ee
String-Xeno <the1flying2gamer345@gmail.com>
2024-10-19 00:53:53 +0300

    should be done, check whatsapp with teacher

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
-import  java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -10,7 +10,9 @@ public class Main {
         int err0rrollbacks = 0;
         for (int i = 13; i <= 19; i++) {
 
+            System.out.println();
             System.out.println("Targil " + i + " is running now!");
+            System.out.println();
             if (i == 13){
                 while (err0rrollbacks != 1) { //I use the while in error cases, the project will rerun
                     System.out.println("Hello! this is gram to ounce and ounce to gram calculator!");
@@ -60,12 +62,72 @@ public class Main {
 
                 System.out.println("Please enter Coding test result: ");
                 int cctest = input.nextInt();
-                double average = (htest + etest + cctest + mtest) / 4;
-                System.out.print("The students average is: ");
+                double average = (double) (htest + etest + cctest + mtest) / 4;
+                System.out.println("The students average is: " + average);
 
+            }
+            if (i == 15) {
+                System.out.println("Hello! this is a calculator to add the tax price to the item (default tax 18%)");
+                System.out.println("Please enter the price of the item you'd like to purchase: ");
+                int price = input.nextInt();
+                double pricewtax = price + price*0.18;
+                System.out.println("The price of the item with tax is: " + pricewtax);
+            }
+            if (i == 16) {
+                System.out.println("seller agent tax calculator");
+                System.out.println("Shirt cost the agent sold them at:");
+                int sold = input.nextInt();
+                System.out.println("How many shirts did he sell?");
+                int amount = input.nextInt();
+                double taxed = amount*0.12 * sold;
+                System.out.print("calculating");
+                for (int la = 0; la <4; la++){
+                    Thread.sleep(550);
+                    System.out.print(".");
+                }
                 System.out.println();
+                System.out.println("the agent's pay is: " + taxed);
+            } else if (i == 17) {
+                System.out.println("Enter monthly salary:");
+                int salary = input.nextInt();
+                double equalspendingmoney = ((salary - salary*0.15) * (2/3))/30;
+                System.out.println("You can spend " + equalspendingmoney + "per day");
+            } else if (i == 18) {
+                System.out.println("How much was the property sold for?");
+                int pr1ce = input.nextInt();
+                System.out.println("Enter the tax % the real estate agent takes");
+                int taxpercent = input.nextInt();
+                System.out.println("Enter expenses %");
+                int expens3s = input.nextInt();
+                System.out.println("Original price: " + pr1ce);
+                double agentcost = pr1ce * taxpercent;
+                System.out.println("Real estate agent cost: " + taxpercent);
+                double expenses = pr1ce * expens3s;
+                System.out.println("other expenses: " + expenses);
+                double finalprice = pr1ce - expens3s - taxpercent;
+                System.out.println("the final profit is: " + finalprice);
+            } else if (i == 19) {
+                //first way
+                System.out.println("enter A value");
+                int a = input.nextInt();
+                System.out.println("enter B value");
+                int b = input.nextInt();
+                int c = a;
+                a = b;
+                b = c;
+                System.out.println("Values swapped! a = " + a + "and value b = " + b);
+                //second way
+                System.out.println("second way");
+                System.out.println("enter A value");
+                int x = input.nextInt();
+                System.out.println("enter B value");
+                int y = input.nextInt();
+                x = x + y;
+                y = x - y;
+                x = x - y;
+                System.out.println("Values swapped, now x = " + x + " and y = " + y);
 
-                System.out.printf("");
+
             }
         }
     }
