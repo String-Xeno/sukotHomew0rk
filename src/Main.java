1e8cb214d414b620b6e691dc042e26961d787701
String-Xeno <the1flying2gamer345@gmail.com>
2024-10-18 19:24:48 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.util.Scanner;
 import  java.util.concurrent.TimeUnit;
 public class Main {
